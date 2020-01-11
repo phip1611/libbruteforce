507ccf024f57a06dac685b37cae5c219b7d50eb8
Philipp Schuster <philipp.schuster@t-systems.com>
2020-01-11 15:43:14 +0100

    notify threads if another one is is done and found a solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
-use std::time::Instant;
-use crate::util::seconds_as_fraction;
-use crate::symbols::{combinations_count};
-use crate::indices::{indices_create, indices_to_string, indices_increment_by};
+
+use crate::indices::{indices_create, indices_increment_by, indices_to_string};
+use crate::symbols::combinations_count;
 use crate::transformation_fns::TransformationFn;
 
 mod indices;
 pub mod symbols;
-mod util;
 pub mod transformation_fns;
 
 /// This function takes a target string( e.g. a MD5-Hash), the alphabet, the max length and tries
@@ -35,11 +34,10 @@ pub fn crack(target: String,
         num_cpus::get() as isize
     } else { 1 };
 
-    // TODO add something to stop threads if one found a solution
-
     // make function parameters ready for sharing between threads
     let alphabet = Arc::from(alphabet);
     let target: Arc<String> = Arc::from(target);
+    let done = Arc::from(AtomicBool::from(false));
 
     let mut handles = vec![];
 
@@ -52,17 +50,39 @@ pub fn crack(target: String,
         let target = Arc::clone(&target);
         let alphabet = Arc::clone(&alphabet);
 
+        let done = done.clone();
+
         // prepare array for thread with right starting index
         indices_increment_by(&alphabet, &mut indices, tid as isize).expect("Increment failed");
 
         // spawn all threads
         let h = thread::spawn(move || {
-            let start_time = Instant::now();
+            // The result that the thread calculated/found
             let mut result = None;
+
+            /// The number after how many iterations the thread looks if another thread
+            /// is already done, so that we can stop further work. We do this only after
+            /// a few millions iterations to keep the overhead low. Tests on my machine
+            /// showed that 1 million iterations take about 1.6s - this should be okay
+            /// because the overhead is not that big
+            const INTERRUPT_COUNT_THRESHOLD: usize = 1_000_000;
+            let mut interrupt_count = INTERRUPT_COUNT_THRESHOLD;
+
             // infinite incrementing; break inside loop if its the right time for
             loop {
+                if interrupt_count > 0 {
+                    interrupt_count -= 1;
+                } else {
+                    interrupt_count = INTERRUPT_COUNT_THRESHOLD;
+                    let done = done.load(Ordering::SeqCst);
+                    if done {
+                        // another thread already found a solution
+                        break;
+                    }
+                }
+
                 let res = indices_increment_by(&alphabet, &mut indices, thread_count);
-                if let Err(_) = res {
+                if res.is_err() {
                     // reached incrementing limit; thread is done
                     break;
                 }
@@ -71,6 +91,8 @@ pub fn crack(target: String,
                 // transform; e.g. hashing
                 let transformed_string = transform_fn(&string);
                 if transformed_string == *target {
+                    // let other threads now we are done
+                    done.store(true, Ordering::SeqCst);
                     result = Some(string);
                 }
             }
@@ -93,10 +115,11 @@ pub fn crack(target: String,
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::symbols::full_alphabet;
     use crate::transformation_fns::identity::NO_HASHING;
     use crate::transformation_fns::sha256::SHA256_HASHING;
+
+    use super::*;
 
     #[test]
     fn test_crack_identity() {
@@ -117,5 +140,4 @@ mod tests {
         let result = result.unwrap();
         assert!(input.eq(&result), "target and cracked result must equal!");
     }
-
 }
--- src/util.rs
+++
@@ -1,6 +0,0 @@
-use std::time::Instant;
-
-pub fn seconds_as_fraction(instant: &Instant) -> f64 {
-    let millis = instant.elapsed().as_millis() as f64;
-    millis / 1000 as f64
-}
