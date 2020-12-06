f2f24efdd9b44598005466806a0a217404e4cead
Philipp Schuster <phip1611@gmail.com>
2020-12-06 23:10:12 +0100

    v2.2.2 build fix

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,13 @@
 //! ```
 //! use libbruteforce::{symbols, transform_fns};
 //! use libbruteforce::CrackParameter;
-//! use simple_logger::SimpleLogger;
+//! // If you want: add a "log"-crate compatibly logger to Cargo.toml
+//! // like "simple_logger".
+//! // use simple_logger::SimpleLogger;
 //!
 //! fn main() {
 //!     // to get information about trace! logs (like progress) on the console
-//!     SimpleLogger::new().init().unwrap();
+//!     // SimpleLogger::new().init().unwrap();
 //!
 //!     let alphabet = symbols::Builder::new().with_lc_letters().with_common_special_chars().build();
 //!     let sha256_hash = String::from("3d7edde33628331676b39e19a3f2bdb3c583960ad8d865351a32e2ace7d8e02d");
@@ -22,9 +24,9 @@
 //!         )
 //!     );
 //!     if res.is_success() {
-//!         println!("Password is: {}", res.solution.unwrap())
+//!          println!("Password is: {}", res.solution.unwrap())
 //!     }
-//! }
+//!}
 //! ```
 
 pub use crack::crack;
